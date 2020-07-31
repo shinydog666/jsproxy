b83ee5800ed986509445a47714e61bee564c92df
shinydog666 <58540850+shinydog666@users.noreply.github.com>
2020-08-01 01:28:31 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -88,10 +88,10 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
+    'https://www.porksjxbkaoznjbskzsxhnhub.com/': {
       redir: 'https://php.net/'
     },
-    'http://haha.com/': {
+    'http://hahhdjjabxnwkzkpxaknsna.com/': {
       content: 'Hello World'
     },
   }
